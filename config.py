92ce19a9cfe40c15040c80f61fc341c2db83a7ae
Sourabh Patil <patilss1610@gmail.com>
2020-11-14 10:21:14 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+customhost = "patientdata.chup90erev8j.ap-south-1.rds.amazonaws.com"
+
+customuser = "admin"
+custompass = "adminpass"
+customdb = "patient"
+
+custombucket = "patientbucket1"
+
+customregion = "ap-south-1"
